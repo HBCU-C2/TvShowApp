25843f6218f8e3ac73c845ad600b24d6ecb15ac4
Marc Aupont <mewsicmasta@gmail.com>
2021-08-08 12:59:35 -0400

    Created model objects and view model

---
+++ TVShowApp/Models/Rating.swift
@@ -0,0 +1,12 @@
+//
+//  Rating.swift
+//  TVShowApp
+//
+//  Created by Marc Aupont on 8/8/21.
+//
+
+import Foundation
+
+struct Rating: Decodable {
+    var average: Double
+}
---
+++ TVShowApp/Models/Schedule.swift
@@ -0,0 +1,13 @@
+//
+//  Schedule.swift
+//  TVShowApp
+//
+//  Created by Marc Aupont on 8/8/21.
+//
+
+import Foundation
+
+struct Schedule: Decodable {
+    var time: String
+    var days: [String]
+}
